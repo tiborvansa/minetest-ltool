dad2adc297f063eeabcd750e820de37010b9bd34
Wuzzy <almikes@aol.com>
2014-07-26 23:22:09 +0200

    Destroy L-system tree sapling on dig
    This is an intermediate solution to this problem:
    After digging a sapling, the metadata is lost
    and when it is placed again, it won't grow a tree.
    It is considered safer to not drop anything at all
    instead to prevent buggy behaviour.
    
    A better solution may be found later.

--- init.lua
+++ init.lua
@@ -33,6 +33,7 @@ minetest.register_node("ltool:sapling", {
 	walkable = false,
 	buildable_to = true,
 	groups = { dig_immediate = 3, not_in_creative_inventory=1 },
+	drop = "",
 	after_place_node = function(pos, placer, itemstack, pointed_thing)
 		-- Transfer metadata and start timer
 		local nodemeta = minetest.get_meta(pos)
