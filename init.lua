6653a1d6c4349bcf7409b4d9a63fdf6091a538c0
Wuzzy <almikes@aol.com>
2015-02-08 00:41:02 +0100

    Bump patch version number

--- init.lua
+++ init.lua
@@ -3,8 +3,8 @@ ltool = {}
 ltool.VERSION = {}
 ltool.VERSION.MAJOR = 1
 ltool.VERSION.MINOR = 0
-ltool.VERSION.PATCH = 0
-ltool.VERSION.STRING = "1.0.0"
+ltool.VERSION.PATCH = 1
+ltool.VERSION.STRING = "1.0.1"
 
 ltool.playerinfos = {}
 ltool.default_edit_fields = {
