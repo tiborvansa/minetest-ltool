245d0b3398ae0403e3710d710d956bac7a68ffa2
Wuzzy <almikes@aol.com>
2016-11-02 02:07:54 +0100

    Remove in_doc group

--- init.lua
+++ init.lua
@@ -41,7 +41,7 @@ minetest.register_node("ltool:sapling", {
 	paramtype2= "wallmounted",
 	walkable = false,
 	buildable_to = true,
-	groups = { dig_immediate = 3, not_in_creative_inventory=1, in_doc=1 },
+	groups = { dig_immediate = 3, not_in_creative_inventory=1, },
 	drop = "",
 	is_ground_content = false,
 	after_place_node = function(pos, placer, itemstack, pointed_thing)
