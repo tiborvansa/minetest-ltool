5efb442de0bee8fba3044118766371a28ca5ad08
Wuzzy <Wuzzy2@mail.ru>
2019-03-20 12:36:45 +0100

    Version 1.6.1

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ ltool = {}
 ltool.VERSION = {}
 ltool.VERSION.MAJOR = 1
 ltool.VERSION.MINOR = 6
-ltool.VERSION.PATCH = 0
+ltool.VERSION.PATCH = 1
 ltool.VERSION.STRING = ltool.VERSION.MAJOR .. "." .. ltool.VERSION.MINOR .. "." .. ltool.VERSION.PATCH
 
 ltool.playerinfos = {}
